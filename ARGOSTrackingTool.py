f31df7a4b59abf39815aaa66b6f2a163ebb63fee
ardathdixon <ardath.dixon@duke.edu>
2020-09-18 15:47:52 -0400

    Initial commit of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Ardath Dixon (ardath.dixon@duke.edu)
+# Date:   Sept 2020
+#--------------------------------------------------------------
